2b5591d7cab68b51b3f12c9c1d4fad77cd5da25e
MuhammadIkram24 <m.ikramch22@gmail.com>
2022-05-06 03:08:23 +0500

    updated files

--- server/routes/users.routes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-const router = express.Router();
-
-import { loginUser, signUpUser } from "../controllers/users.controllers.js";
-
-router.post("/login", loginUser);
-router.post("/signup", signUpUser);
-
-export default router;
